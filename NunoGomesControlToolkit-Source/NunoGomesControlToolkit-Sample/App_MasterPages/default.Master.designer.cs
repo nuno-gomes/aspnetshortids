8d69c37dac23fa953bc62ec1a4fec9e27e167e0e
nmgomes_cp <SND\nmgomes_cp>
2011-08-22 22:06:42 +0000

    Added a LinkButton at MasterPage and hook the Click event.

--- NunoGomesControlToolkit-Source/NunoGomesControlToolkit-Sample/App_MasterPages/default.Master.designer.cs
+++ NunoGomesControlToolkit-Source/NunoGomesControlToolkit-Sample/App_MasterPages/default.Master.designer.cs
@@ -49,6 +49,15 @@ namespace WebApp.App_MasterPages {
         protected global::System.Web.UI.WebControls.Menu menu;
         
         /// <summary>
+        /// MyLinkButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton MyLinkButton;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
