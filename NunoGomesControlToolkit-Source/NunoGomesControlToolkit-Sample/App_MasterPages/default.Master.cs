db31fd4f901727073280a0bdcc6f6b07e1a2a4ee
nmgomes_cp <17520661+nuno-gomes@users.noreply.github.com>
2011-08-22 00:06:14 +0000

    Added a MasterPage and change all sample pages to use it.

---
+++ NunoGomesControlToolkit-Source/NunoGomesControlToolkit-Sample/App_MasterPages/default.Master.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApp.App_MasterPages
+{
+    public partial class _default : NunoGomes.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
